86bdf786cc47ab22451a1f4c4d64f89235e84f81
Tut-MacBook <tutkowski@Tut-MacBooks-MacBook-Pro.local>
2020-10-15 11:40:34 -0600

    Allow flexible storing of strings based on length.

--- src/com/datastax/serde/TextType.java
+++ src/com/datastax/serde/TextType.java
@@ -3,8 +3,10 @@ package com.datastax.serde;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class TextType implements AbstractType<String> {
+    public static final int MAX_SERIALIZED_STRING_LENGTH = 2^31 + 1;
 
     public static TextType instance = new TextType();
 
@@ -20,6 +22,12 @@ public class TextType implements AbstractType<String> {
         // For the sake of simplicity, I am assuming that neither the variable 'value' nor the variable 'out'
         // has the value null.
 
+        if (sizeof(value) > MAX_SERIALIZED_STRING_LENGTH) {
+            throw new IOException("The serializable size of a string must not exceed " +
+                    MAX_SERIALIZED_STRING_LENGTH + " bytes.");
+        }
+
+        out.writeInt(value.getBytes().length);
         out.writeBytes(value);
     }
 
@@ -27,16 +35,23 @@ public class TextType implements AbstractType<String> {
     public int sizeof(String value) {
         // For the sake of simplicity, I am assuming the value of the variable 'value' is not null.
 
-        return value.getBytes().length;
+        return Integer.BYTES + value.getBytes().length;
     }
 
     @Override
     public String deserialize(DataInput in) throws IOException {
         // For the sake of simplicity, I am assuming the value of the variable 'in' is not null.
 
-        String line = in.readLine();
+        int length = in.readInt();
+        byte[] bytes = new byte[length];
 
-        return line != null ? line : "";
+        for (int i = 0; i < length; i++) {
+            byte b = in.readByte();
+
+            bytes[i] = b;
+        }
+
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 
     @Override
